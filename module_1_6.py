be49e428567646aab044a97290d1974ac1873db1
Antontonik86 <antontonik86@gmail.com>
2024-08-13 13:45:02 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,18 @@
+my_dict = {'Anton': 1986, 'Max': 1982, "Ivan": 1980}
+print(my_dict)
+print(my_dict['Anton'])
+print(my_dict.get('Den'))
+my_dict.update({'Serg': 1987,
+                'Vlad': 1997})
+print(my_dict)
+del my_dict['Vlad']
+print(my_dict)
+
+my_set = {12, 25, 35, 45, 50, 4.31, 35, 52, 531, 'String', True, (12, 22, 32)}
+print(my_set)
+my_set.add(6)
+my_set.add('Hi')
+print(my_set)
+my_set.discard((12, 22, 32))
+print(my_set)
+print('Modified set:', my_set)
